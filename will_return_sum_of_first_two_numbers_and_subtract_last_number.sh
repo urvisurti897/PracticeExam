cddba369a574f75dc03954dce01b236b76d4a18b
Urvi <urvisurti897@gmail.com>
2025-01-28 15:00:44 -0500

    Updating values for adding first two numbers and subtracting third numbers

--- will_return_sum_of_first_two_numbers_and_subtract_last_number.sh
+++ will_return_sum_of_first_two_numbers_and_subtract_last_number.sh
@@ -1,9 +1,6 @@
-will_return_sum_of_first_two_numbers_and_subtract_last_number.sh
-
-
 #!/bin/bash
 
-# file: will_return_sum_of_two_numbers_correctly.sh
+# file: sum_upto_two_numbers_and_subtract_last_number.sh
 
 RESULT=$(./sum_upto_two_numbers_and_subtract_last_number.sh 5 4 2 )
  
